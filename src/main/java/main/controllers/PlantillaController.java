368b37e3772bffeac1317b5ca5c65156047da053
JuanManuelDS <juanmadesenzi@gmail.com>
2022-12-19 19:36:16 +0100

    plantillas controller modificado

--- src/main/java/main/controllers/PlantillaController.java
+++ src/main/java/main/controllers/PlantillaController.java
@@ -3,6 +3,7 @@ package main.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import main.dto.Plantilla;
@@ -19,6 +21,7 @@ import main.services.UsuarioService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class PlantillaController {
 
 	@Autowired
